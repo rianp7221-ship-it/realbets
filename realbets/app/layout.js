51847cda8f9b070305a8995a26ea405180eda4fc
rianp7221-ship-it <rianp7221@gmail.com>
2026-05-24 23:39:55 -0300

    Update layout.js

---
+++ realbets/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'REALSPORTS.TOP',
+  description: 'Sports Betting Platform',
+}
+
+import './globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
